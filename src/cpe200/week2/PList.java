cdc9ddfaa99296a6f66fd6f40b14e1b8a645406c
Peerawit Naprae <peerawit503@gmail.com>
2016-08-30 12:57:47 +0700

    1043

--- src/cpe200/week2/PList.java
+++ src/cpe200/week2/PList.java
@@ -1,9 +1,10 @@
 package cpe200.week2;
 
 public class PList {
-
-    public PList() { head = tail = null; }
-
+    public PList()
+    {
+        head = tail = null;
+    }
     public void pushToHead(char i) {
         head = new PNode(i, head, null);
         if (tail==null)
@@ -12,15 +13,24 @@ public class PList {
             head.next.prev = head;
         size++;
     }
-
     public void pushToTail(char i) {
-        // implement your code here
+        PNode temp = new PNode(i,head, null);
+        if (tail==null)
+        {
+            tail = head;
+        }
+        else
+        {
+            temp.prev = tail;
+            temp.next = null;
+            tail.next = temp;
+            tail = temp;
+            size++;
+        }
     }
-
     public char popHead() {
         char i=head.data;
         PNode tmp = head;
-
         if (head==tail)
             head = tail = null;
         else {
@@ -28,39 +38,63 @@ public class PList {
             head.prev = null;
             tmp.next = null;
         }
-
         size--;
-
         return i;
     }
-
     public char popTail() {
-        // implement your code here
+        char n = tail.data;
+        PNode temp;
+        if (tail.prev == null) {
+            temp = tail;
+            size--;
+        }
+        else {
+            temp = tail;
+            tail = temp.prev;
+            tail.next = null;
+            size--;
+        }
+        return n;
     }
-
     public boolean search(char i) {
-        // implement your code here
+        PNode temp;
+        temp = head;
+        if (temp.data == i)
+        {
+            return true;
+        }
+        while (temp.next.data != i) {
+            temp = temp.next;
+            if (temp.next == null) {
+                return false;
+            }
+        }
+        return true;
     }
-
-    public boolean isEmpty() { return (head == null); }
-
+    public boolean isEmpty()
+    {
+        return (head == null);
+    }
     public void printForward() {
         PNode tmp = head;
-
-        while (tmp != null) {
-            // implement your code here
+        while (tmp != null)
+        {
+            System.out.println(tmp.data) ;
+            tmp = tmp.next;
         }
-        System.out.println();
     }
-
     public void printBackward() {
-        // implement your code here
+        PNode tmp = tail;
+        while (tmp != null)
+        {
+            System.out.println(tmp.data) ;
+            tmp = tmp.prev;
+        }
     }
-
-    public int getSize() {
+    public int getSize()
+    {
         return size;
     }
-
     private PNode head, tail;
     private int size=0;
 }
